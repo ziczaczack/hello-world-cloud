cc89a4b46a3951ff6876d7986388ce2e371abf06
ziczaczack <ziczaczack@gmail.com>
2024-07-09 21:25:45 +0800

    thrid commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template("index.html")
+def hello():    
+    return render_template("homepage.html")
 
 if __name__ == '__main__':
     app.run()
