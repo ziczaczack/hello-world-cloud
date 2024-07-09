7511d4aa5d9b47435857942985f99c5d168499a5
ziczaczack <ziczaczack@gmail.com>
2024-07-09 20:56:08 +0800

    Fifth commit

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    num1 = request.args.get('num1', default='0', type=int)
-    num2 = request.args.get('num2', default='0', type=int)
-    
-    # Perform addition
-    result = num1 + num2
-    
-    return f'The sum of {num1} and {num2} is {result}'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
